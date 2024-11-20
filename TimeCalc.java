5c67b1aa547522dd36024ddc43b8a164eb290590
GayaHarlev <gaia.harlev@gmail.com>
2024-11-20 17:23:35 +0200

    solution

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,25 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        // קליטת הקלט משורת הפקודה
+        String time = args[0]; // השעה בפורמט hh:mm
+        int minutesToAdd = Integer.parseInt(args[1]); // ממיר את מספר הדקות למספר שלם
+
+        // שימוש ב-substring לחילוץ השעות והדקות
+        int hours = Integer.parseInt(time.substring(0, 2)); // השעות
+        int minutes = Integer.parseInt(time.substring(3, 5)); // הדקות
+
+        // חישוב סך כל הדקות
+        int totalMinutes = (hours * 60) + minutes + minutesToAdd;
+
+        // חישוב השעה והדקות החדשות
+        int newHours = (totalMinutes / 60) % 24; // חלוקה ב-60 ואז מודולו 24 לשעה החדשה
+        int newMinutes = totalMinutes % 60; // שארית מהחלוקה ב-60 לדקות החדשות
+
+        // עיצוב הפלט עם אפסים מובילים
+        String formattedHours = (newHours < 10) ? "0" + newHours : String.valueOf(newHours);
+        String formattedMinutes = (newMinutes < 10) ? "0" + newMinutes : String.valueOf(newMinutes);
+
+        // הדפסת התוצאה
+        System.out.println(formattedHours + ":" + formattedMinutes);
     }
 }
--- TimeCalc1.java
+++ TimeCalc1.java
@@ -1,24 +1,25 @@
-public class TimeCalc1 {
+public class TimeCalc {
     public static void main(String[] args) {
-      
-        String time = args[0]; 
-        int minutesToAdd = Integer.parseInt(args[1]); 
+        // קליטת הקלט משורת הפקודה
+        String time = args[0]; // השעה בפורמט hh:mm
+        int minutesToAdd = Integer.parseInt(args[1]); // ממיר את מספר הדקות למספר שלם
 
-        
+        // שימוש ב-substring לחילוץ השעות והדקות
         int hours = Integer.parseInt(time.substring(0, 2)); // השעות
         int minutes = Integer.parseInt(time.substring(3, 5)); // הדקות
 
-        // חישוב הדקות
+        // חישוב סך כל הדקות
         int totalMinutes = (hours * 60) + minutes + minutesToAdd;
 
-        // חישוב השעה והדקות 
-        int newHours = (totalMinutes / 60) % 24; // חישוב שעה לפי הנוסחה הנתונה
-        int newMinutes = totalMinutes % 60; // חישוב דקות לפי הנוסחה
+        // חישוב השעה והדקות החדשות
+        int newHours = (totalMinutes / 60) % 24; // חלוקה ב-60 ואז מודולו 24 לשעה החדשה
+        int newMinutes = totalMinutes % 60; // שארית מהחלוקה ב-60 לדקות החדשות
 
-        String formattedHours = (newHours < 10 ? "0" + newHours : "" + newHours);
-        String formattedMinutes = (newMinutes < 10 ? "0" + newMinutes : "" + newMinutes);
+        // עיצוב הפלט עם אפסים מובילים
+        String formattedHours = (newHours < 10) ? "0" + newHours : String.valueOf(newHours);
+        String formattedMinutes = (newMinutes < 10) ? "0" + newMinutes : String.valueOf(newMinutes);
 
-        // הדפסה
+        // הדפסת התוצאה
         System.out.println(formattedHours + ":" + formattedMinutes);
     }
 }
